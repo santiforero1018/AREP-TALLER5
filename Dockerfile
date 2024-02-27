2238f832bab89e28c7ae28c47a3a08bf03e7c8b0
unknown <santiago.forero-y@labinfo.is.escuelaing.edu.co>
2024-02-27 16:39:36 -0500

    estrucutra taller 5

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM openjdk:17
+
+WORKDIR /usrapp/bin
+
+ENV PORT 57000
+
+COPY /target/classes /usrapp/bin/classes
+COPY /target/dependency /usrapp/bin/dependency
+
+CMD ["java","-cp","./classes:./dependency/*","edu.eci.arep.taller5.SparkWebServer"]
---
+++ src/main/java/edu/eci/arep/taller5/SparkWebServer.java
@@ -0,0 +1,20 @@
+package edu.eci.arep.taller5;
+
+import static spark.Spark.port;
+import static spark.Spark.get;
+
+public class SparkWebServer {
+    
+    public static void main(String[] args){
+          port(getPort());
+          get("hello", (req,res) -> "Hello Docker!");
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+    
+}
