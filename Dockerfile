c27883001bfc917d5b2f438369e1510b9d903712
Santiago Forero Yate <santiago.forero-y@mail.escuelaing.edu.co>
2024-02-28 23:23:25 -0500

    finalización taller

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,5 @@ ENV PORT 57000
 COPY /target/classes /usrapp/bin/classes
 COPY /target/dependency /usrapp/bin/dependency
 
+
 CMD ["java","-cp","./classes:./dependency/*","edu.eci.arep.taller5.SparkWebServer"]
--- src/main/java/edu/eci/arep/taller5/SparkWebServer.java
+++ src/main/java/edu/eci/arep/taller5/SparkWebServer.java
@@ -1,12 +1,12 @@
 package edu.eci.arep.taller5;
 
 import static spark.Spark.port;
-
-import edu.eci.arep.taller5.server.WebServer;
+import static spark.Spark.staticFileLocation;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import spark.Spark;
 
 import static spark.Spark.get;
 
@@ -17,11 +17,74 @@ public class SparkWebServer {
         get("hello", (req, res) -> "Hello Docker!");
         get("/coseno", (req, res) -> {
             try {
-                return new String(Files.readAllBytes(Paths.get("target/classes/public/static/cos.html")));
+                return new String(Files.readAllBytes(Paths.get("/usrapp/bin/classes/public/static/cos.html")));
             } catch (Exception e) {
                 // TODO: handle exception
                 e.printStackTrace();
             }
+            return null;
+        });
+        get("/seno", (req, res) -> {
+            try {
+                return new String(Files.readAllBytes(Paths.get("/usrapp/bin/classes/public/static/sin.html")));
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+            return null;
+        });
+        get("/magnitud", (req, res) -> {
+            try {
+                return new String(Files.readAllBytes(Paths.get("/usrapp/bin/classes/public/static/magnitud.html")));
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+            return null;
+        });
+        get("/palindromo", (req, res) -> {
+            try {
+                return new String(Files.readAllBytes(Paths.get("/usrapp/bin/classes/public/static/palindromo.html")));
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+            return null;
+        });
+
+        get("/result", (req, res) -> {
+            try {
+                String op = req.queryParams("op"), resp = "", chain = req.queryParams("chain");
+                Double param1 = req.queryParams("num1") != null ? Double.parseDouble(req.queryParams("num1")) : null;
+
+                Double param2 = req.queryParams("num2") != null ? Double.parseDouble(req.queryParams("num2")) : null;
+                switch (op) {
+                    case "sin":
+                        resp += Math.sin(param1);
+                        return new String(Files.readAllBytes(Paths.get("/usrapp/bin/classes/public/static/result.html")))
+                                .replace("{operation}", op).replace("{result}", resp);
+                    case "cos":
+                        resp += Math.cos(param1);
+                        return new String(Files.readAllBytes(Paths.get("/usrapp/bin/classes/public/static/result.html")))
+                                .replace("{operation}", op).replace("{result}", resp);
+                    case "mag":
+                        resp += Math.sqrt(param1*param1+param2*param2);
+                        return new String(Files.readAllBytes(Paths.get("/usrapp/bin/classes/public/static/result.html")))
+                                .replace("{operation}", op).replace("{result}", resp);
+                    case "palindromo":
+                        resp += chain.equals(new StringBuilder(chain).reverse().toString());
+                        return new String(Files.readAllBytes(Paths.get("/usrapp/bin/classes/public/static/result.html")))
+                                .replace("{operation}", op).replace("{result}", resp);
+
+                    default:
+                        resp = "";
+                        break;
+                }
+            } catch (Exception e) {
+                // TODO: handle exception
+                e.printStackTrace();
+            }
+
             return null;
         });
     }
